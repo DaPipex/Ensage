2e98192475c1dde2d707eb31a5099dc20558a17b
Felipe <felipgg@gmail.com>
2015-10-21 01:14:42 -0300

    DrowRanger WIP

---
+++ PippyDrowRanger/MinionAttackData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PippyDrowRanger
+{
+    class MinionAttackData
+    {
+        public MinionAttackData(string creepName)
+    }
+}
